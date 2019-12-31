938fc6f5fdbdb47c6a0360b197d046d794634499
anyusernameworks <patlymlatun@gmail.com>
2019-12-31 10:51:48 -0800

    moved Markup impl to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,15 @@ pub enum Markup {
    Text(String)
 }
 
+impl std::fmt::Display for Markup {
+   fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+      match self {
+         Markup::Tag(el) => write!(f, "{}", el),
+         Markup::Text(text) => write!(f, "{}", text)
+      }
+   }
+}
+
 pub enum TagType {
    Paired(Vec<Markup>),
    Unpaired,
--- src/tag.rs
+++ src/tag.rs
@@ -1,14 +1,5 @@
 use super::*;
 
-impl std::fmt::Display for Markup {
-   fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-      match self {
-         Markup::Tag(el) => write!(f, "{}", el),
-         Markup::Text(text) => write!(f, "{}", text)
-      }
-   }
-}
-
 pub struct Tag {
    tag_name: TagName,
    attributes: Vec<Attr>,
