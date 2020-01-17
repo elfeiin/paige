a551142a0fe909646025d90fa18bc5ff06b0d5ca
anyusernameworks <patlymlatun@gmail.com>
2020-01-16 20:01:51 -0800

    made some formatting changes to example.

--- examples/index_with_nav.rs
+++ examples/index_with_nav.rs
@@ -1,51 +1,52 @@
 use paige::*;
 
 fn css() -> El {
-   El::text(
-"* {
-	margin: 0 0 0 0;
-	background: #12121225;
-	border: 1px solid #333;
-	color: #999;
-}
+   El::text("
+         * {
+            margin: 0 0 0 0;
+            background: #12121225;
+            border: 1px solid #333;
+            color: #999;
+         }
 
-html {
-	height: 100%;
-	border: none;
-}
+         html {
+            height: 100%;
+            border: none;
+         }
 
-body {
-	width: 100%;
-	height: 100%;
-	position: fixed;
-	background: linear-gradient(
-		45deg,
-		#27232f,
-		#000 45%,
-		#27232f
-	);
-	border: none;
-}
+         body {
+            width: 100%;
+            height: 100%;
+            position: fixed;
+            background: linear-gradient(
+               45deg,
+               #27232f,
+               #000 45%,
+               #27232f
+            );
+            border: none;
+         }
 
-	#navigation {
-		margin: 1cm 0 0 2cm;
-		width: 14cm;
-		height: 1cm;
-	}
-	
-		a {
-			border: none;
-		}
-	
-	#main {
-		margin: 1cm 1.5cm 1.5cm 2cm;
-		width: auto;
-		height: auto;
-	}
-	
-.nav {
-	line-height: 1cm;
-}")
+            #navigation {
+               margin: 1cm 0 0 2cm;
+               width: 14cm;
+               height: 1cm;
+            }
+            
+               a {
+                  border: none;
+               }
+            
+            #main {
+               margin: 1cm 1.5cm 1.5cm 2cm;
+               width: auto;
+               height: auto;
+            }
+            
+         .nav {
+            line-height: 1cm;
+         }
+")
 }
 
 fn nav() -> El {
